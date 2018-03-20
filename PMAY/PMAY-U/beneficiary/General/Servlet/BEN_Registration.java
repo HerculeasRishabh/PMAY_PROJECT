8f5b60e3ed5d6ff0a84f9be8abe5c2606c2e006c
rishabhojha <rishabh.education.123@gmail.com>
2018-03-20 10:24:44 +0530

    First Commit

---
+++ PMAY/PMAY-U/beneficiary/General/Servlet/BEN_Registration.java
@@ -0,0 +1,109 @@
+package beneficiary.General.Servlet;
+
+import beneficiary.General.connection.*;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class BEN_Registration
+ */
+@WebServlet("/BEN_Registration")
+@MultipartConfig(fileSizeThreshold=1024*1024*2,
+maxFileSize=1024*1024*5)
+public class BEN_Registration extends HttpServlet {
+    private static final String SAVE_DIR="ImagesBeneficiary";
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BEN_Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    response.setContentType("text/html");
+         String savePath = "/Users/rishabhojha/Documents/workspace/PMAY/WebContent/PMAY-U" + File.separator + SAVE_DIR;
+         File fileSaveDir=new File(savePath);
+         if(!fileSaveDir.exists()){
+             fileSaveDir.mkdir();
+         }
+         
+         String STATE = request.getParameter ("stateName");
+         String DISTRICT = request.getParameter ("districtName");
+         String CITY = request.getParameter ("cityName");
+         String WARD = request.getParameter ("wardName");
+         String SLUM_NAME = request.getParameter ("slumName");
+         String AREA_NAME = request.getParameter ("areaName");
+         byte HFA_VERTICAL = Byte.parseByte(request.getParameter("compName")); 
+         String FMLY_HEAD_NM = request.getParameter ("famHeadName");
+         byte SEX = Byte.parseByte(request.getParameter ("gender"));
+         String FATHERS_NAME = request.getParameter ("fathersName");
+         byte FMLY_HEAD_AGE = Byte.parseByte(request.getParameter ("headAge"));
+         String HOUSE_NO = request.getParameter ("addressHouseNumber");
+         String STREET = request.getParameter ("addressStreetName");
+         String MOBILE_NO = request.getParameter ("mobileNumber");
+         byte OWNERSHIP = Byte.parseByte(request.getParameter ("ownershipDetails"));
+         byte HOUSE_TYPE = Byte.parseByte(request.getParameter ("houseType"));
+         byte NO_ROOMS = Byte.parseByte(request.getParameter ("noOfRooms"));
+         String AADHAR_CARD = request.getParameter ("aadharNumber");
+         byte RELIGION = Byte.parseByte(request.getParameter ("religionName"));
+         byte CASTE = Byte.parseByte(request.getParameter ("casteName"));
+         String BANK_NM = request.getParameter ("bankName");
+         String BANK_ACC_NO = request.getParameter ("accountNumber");
+         String BRANCH_NM = request.getParameter ("branchName");
+         byte YEARS_STAY = Byte.parseByte(request.getParameter ("yearStay"));
+         BigDecimal SIZE_DU = new BigDecimal (request.getParameter ("dwellingUnits"));
+         byte DISABLE = Byte.parseByte(request.getParameter ("disability"));
+         byte MARITAL_STATUS = Byte.parseByte(request.getParameter ("maritalStatus"));
+         byte OWNS_HOUSE_LAND = Byte.parseByte(request.getParameter ("ownsHouseLand"));
+         byte EMPLOYMENT = Byte.parseByte(request.getParameter ("employmentStatus"));;
+         BigDecimal AVG_MONTHLY_INC = new BigDecimal (request.getParameter ("monthlyIncome"));
+         String BPL_CARD_NO = request.getParameter ("BLP_Card_No");
+         byte HOUSING_REQ = Byte.parseByte(request.getParameter ("housingRequirement"));
+         Part IMG_PATH = request.getPart ("imageBeneficiary");
+         //String DGTL_SIGN_ULB = request.getParameter ("");
+         String DGTL_SIGN_ULB = "Test1";
+         
+         String time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Timestamp(System.currentTimeMillis()));
+         IMG_PATH.write(savePath + File.separator + time + ".jpg" );
+         String FMLY_HEAD_IMG_PATH= savePath + File.separator + time + ".jpg";
+         
+         BEN_GNRL_Registration obj = new BEN_GNRL_Registration();
+         
+         obj.beneficiaryGeneralInsert(STATE, DISTRICT, CITY, WARD, SLUM_NAME, AREA_NAME, HFA_VERTICAL, FMLY_HEAD_NM, SEX, FATHERS_NAME,
+                 FMLY_HEAD_AGE, HOUSE_NO, STREET, MOBILE_NO, OWNERSHIP, HOUSE_TYPE, NO_ROOMS, AADHAR_CARD, RELIGION, CASTE, BANK_NM, 
+                 BANK_ACC_NO, BRANCH_NM, YEARS_STAY, SIZE_DU, DISABLE, MARITAL_STATUS, OWNS_HOUSE_LAND, EMPLOYMENT, AVG_MONTHLY_INC, 
+                 BPL_CARD_NO, HOUSING_REQ, FMLY_HEAD_IMG_PATH, DGTL_SIGN_ULB);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ PMAY/PMAY-U/beneficiary/General/connection/BEN_GNRL_Registration.java
@@ -0,0 +1,134 @@
+package beneficiary.General.connection;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+
+/**
+ * 
+ * @author rishabhojha
+ *
+ * This class contains functions that deal with the
+ * general-BENEFICIARY related data in the database 
+ */
+
+public class BEN_GNRL_Registration {
+    
+    //References Declaration
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    
+    //Constant String for JDBC 
+    static final String DRIVER = "com.mysql.jdbc.Driver";
+    static final String USER = "root";
+    static final String PWD = "root";
+    static final String CON_URL = "jdbc:mysql://localhost:3306/PMAY_U";
+    
+    //Query variables
+    String sql = null;
+    int counter = 0;
+    
+    /**
+     * @author rishabhojha
+     * 
+     * Default constructor to establish connection with the database
+     */
+    public BEN_GNRL_Registration() {
+        
+        try{
+            //STEP 1 - Load the Driver
+            Class.forName (DRIVER);
+            
+            //STEP 2 - Establish the connection
+            conn = DriverManager.getConnection(CON_URL, USER, PWD);
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+
+    
+     /////////////////////////////////////// INSERT QUERIES ON DATABASE ////////////////////////////////
+    
+    
+    /**
+     * @author rishabhojha
+     * 
+     * This function inserts the data of a NEW BENEFICIARY 
+     * into the BEN_SURVEY_GNRL table
+     * 
+     * @param 
+     */
+    public void beneficiaryGeneralInsert (String STATE, String DISTRICT, String CITY, String WARD, String SLUM_NAME, String AREA_NAME, 
+            byte HFA_VERTICAL, String FMLY_HEAD_NM, byte SEX, String FATHERS_NAME, byte fMLY_HEAD_AGE, String HOUSE_NO, String STREET, 
+            String MOBILE_NO, byte oWNERSHIP, byte hOUSE_TYPE, byte nO_ROOMS, String AADHAR_CARD, byte rELIGION, byte cASTE, String BANK_NM, 
+            String BANK_ACC_NO, String BRANCH_NM, byte yEARS_STAY, BigDecimal SIZE_DU, byte dISABLE, byte mARITAL_STATUS, byte oWNS_HOUSE_LAND, 
+            byte eMPLOYMENT, BigDecimal AVG_MONTHLY_INC, String BPL_CARD_NO, byte hOUSING_REQ, String FMLY_HEAD_IMG_PATH, String DGTL_SIGN_ULB){
+        
+        sql = "INSERT INTO BEN_SURVEY_GNRL ( "
+                +"STATE, DISTRICT, CITY, WARD, SLUM_NAME, AREA_NAME, HFA_VERTICAL, FMLY_HEAD_NM, SEX, FATHERS_NAME, FMLY_HEAD_AGE,"
+                +"HOUSE_NO, STREET, MOBILE_NO, OWNERSHIP, HOUSE_TYPE, NO_ROOMS, AADHAR_CARD, RELIGION, CASTE, BANK_NM, BANK_ACC_NO," 
+                +"BRANCH_NM, YEARS_STAY, SIZE_DU, DISABLE, MARITAL_STATUS, OWNS_HOUSE_LAND, EMPLOYMENT, AVG_MONTHLY_INC, "
+                +"BPL_CARD_NO, HOUSING_REQ, FMLY_HEAD_IMG_PATH, DGTL_SIGN_ULB"
+                +") VALUES ("
+                +"?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?"
+                +");";
+        try {
+            pstmt = conn.prepareStatement(sql);
+            
+            pstmt.setString(1, STATE);
+            pstmt.setString(2, DISTRICT);
+            pstmt.setString(3, CITY);
+            pstmt.setString(4, WARD);
+            pstmt.setString(5, SLUM_NAME);
+            pstmt.setString(6, AREA_NAME);
+            pstmt.setByte(7, HFA_VERTICAL);
+            pstmt.setString(8, FMLY_HEAD_NM);
+            pstmt.setByte(9, SEX);
+            pstmt.setString(10, FATHERS_NAME);
+            pstmt.setByte(11, fMLY_HEAD_AGE);
+            pstmt.setString(12, HOUSE_NO);
+            pstmt.setString(13, STREET);
+            pstmt.setString(14, MOBILE_NO);
+            pstmt.setByte(15, oWNERSHIP);
+            pstmt.setByte(16, hOUSE_TYPE);
+            pstmt.setByte(17, nO_ROOMS);
+            pstmt.setString(18, AADHAR_CARD);
+            pstmt.setByte(19, rELIGION);
+            pstmt.setByte(20, cASTE);
+            pstmt.setString(21, BANK_NM);
+            pstmt.setString(22, BANK_ACC_NO);
+            pstmt.setString(23, BRANCH_NM);
+            pstmt.setByte(24, yEARS_STAY);
+            pstmt.setBigDecimal(25, SIZE_DU);
+            pstmt.setByte(26, dISABLE);
+            pstmt.setByte(27, mARITAL_STATUS);
+            pstmt.setByte(28, oWNS_HOUSE_LAND);
+            pstmt.setByte(29, eMPLOYMENT);
+            pstmt.setBigDecimal(30, AVG_MONTHLY_INC);
+            pstmt.setString(31, BPL_CARD_NO);
+            pstmt.setByte(32, hOUSING_REQ);
+            pstmt.setString(33, FMLY_HEAD_IMG_PATH);
+            pstmt.setString(34, DGTL_SIGN_ULB);
+            
+            counter = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}//End of class BEN_GNRL_Registraiton
